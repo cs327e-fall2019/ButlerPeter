a175565408386fe9a4d12c310a8ec17d361841c9
Butler Bourgeois <butlerbourgeois@Butlers-MacBook-Air.local>
2019-10-04 21:28:13 -0500

    added transform files and updated join-queries for Milestone 4

--- join-queries.sql
+++ join-queries.sql
@@ -1,41 +1,41 @@
 -- returns name and passer rating of NFL players with a career passer rating greater than 100 that attend college at Texas
 select b.name, p.passer_rating
-from nfl_stats_staging.Basic_Stats as b
-join nfl_stats_staging.Passing_Career_Stats as p on b.player_id = p.player_id
+from nfl_stats_modeled.Basic_Stats as b
+join nfl_stats_modeled.Passing_Career_Stats as p on b.player_id = p.player_id
 where p.passer_rating > 100 and b.college = "Texas"
 order by p.passer_rating desc
 
 -- returns name, height, and rushing attempts per game of runningbacks under 6 feet tall with more than 15 rushing attempts per game
 select b.name, b.height__inches, r.rushing_attempts_per_game
-from nfl_stats_staging.Basic_Stats as b
-join nfl_stats_staging.Rushing_Career_Stats as r on b.player_id = r.player_id
+from nfl_stats_modeled.Basic_Stats as b
+join nfl_stats_modeled.Rushing_Career_Stats as r on b.player_id = r.player_id
 where r.rushing_attempts_per_game > 15 and b.height__inches < 72
 order by b.height__inches
 
 -- returns name, weight, and passer rating of Quarterbacks that weigh 220+ lbs and have a passer rating greater than 100
 select b.name, b.weight__lbs, p.passer_rating
-from nfl_stats_staging.Basic_Stats as b
-join nfl_stats_staging.Passing_Career_Stats as p on b.player_id = p.player_id
+from nfl_stats_modeled.Basic_Stats as b
+join nfl_stats_modeled.Passing_Career_Stats as p on b.player_id = p.player_id
 where p.passer_rating > 100 and b.weight__lbs >= 220
 order by p.passer_rating desc
 
 -- returns name, age, and games played of quarterbacks age 35 and up including null values under games played
 select b.name, b.age, c.games_played
-from nfl_stats_staging.Basic_Stats as b
-full join nfl_stats_staging.Passing_Career_Stats as c on b.player_id = c.player_id
+from nfl_stats_modeled.Basic_Stats as b
+full join nfl_stats_modeled.Passing_Career_Stats as c on b.player_id = c.player_id
 where b.age >= 35
 order by age desc
 
 -- returns name, weight, rushing attempts per game of runningbacks that weigh 220+ lbs from greatest to least rushing attempts per game including null values under rushing attempts per game
 select b.name, b.weight__lbs, c.rushing_attempts_per_game
-from nfl_stats_staging.Basic_Stats as b
-full join nfl_stats_staging.Rushing_Career_Stats as c on b.player_id = c.player_id
+from nfl_stats_modeled.Basic_Stats as b
+full join nfl_stats_modeled.Rushing_Career_Stats as c on b.player_id = c.player_id
 where b.weight__lbs >= 220
 order by c.rushing_attempts_per_game desc
 
 -- returns name and receiving yards per game of quarterbacks ordered from greatest to least receiving yards per game
 select b.name, c.yards_per_game
-from nfl_stats_staging.Basic_Stats as b
-full join nfl_stats_staging.Receiving_Career_Stats as c on b.player_id = c.player_id
+from nfl_stats_modeled.Basic_Stats as b
+full join nfl_stats_modeled.Receiving_Career_Stats as c on b.player_id = c.player_id
 where b.position = "QB"
 order by c.yards_per_game desc
---
+++ transforms.sql
@@ -0,0 +1,53 @@
+-- creates new Basic Stats table with the same entries as original Basic Stats table
+create table nfl_stats_modeled.Basic_Stats as
+select *
+from nfl_stats_staging.Basic_Stats b
+order by b.player_id
+
+-- creates a new Defensive Career Stats table with only necessary defensive stats and player_id, year as a primary key
+create table nfl_stats_modeled.Defensive_Career_Stats as 
+select distinct c.player_id, b.year, b.total_tackles, b.solo_tackles, b.assisted_tackles, b.sacks, b.safties, b.passes_defended, b.ints, b.ints_for_tds, b.int_yards, b.yards_per_int, b.longest_int_return
+from nfl_stats_staging.Defensive_Career_Stats as b,
+(select distinct player_id, year from nfl_stats_staging.Defensive_Career_Stats) as c
+where b.player_id = c.player_id and b.year = c.year
+order by c.player_id, b.year
+
+-- creates new Passing Career Stats table with only passing stats and player_id, year as a primary key
+create table nfl_stats_modeled.Passing_Career_Stats as 
+select distinct c.player_id, b.year, b.passes_attempted, b.games_played , b.passes_completed, b.completion_percentage, b.pass_attempts_per_game, b.passing_yards, b.passing_yards_per_attempt, b.passing_yards_per_game , b.td_passes , b.percentage_of_tds_per_attempts , b.ints , b.int_rate, b.longest_pass, b.passes_longer_than_20_yards, b.passes_longer_than_40_yards, b.sacks, b.sacked_yards_lost, b.passer_rating
+from nfl_stats_staging.Passing_Career_Stats as b,
+(select distinct player_id, year from nfl_stats_staging.Passing_Career_Stats) as c
+where b.player_id = c.player_id and b.year = c.year
+order by c.player_id, b.year
+
+-- creates new Quarterback Game Logs table with only relevent game stats and uses player_id, year, season, game, week as a primary key;
+create table nfl_stats_modeled.Quarterback_Game_Logs as 
+select distinct c.player_id, b.year, b.season , b.week, b.game_date, b.home_or_away, b.opponent, b.outcome, b.score , b.games_played , b.games_started , b.passes_completed , b.passes_attempted , b.completion_percentage , b.passing_yards , b.passing_yards_per_attempt , b.td_passes , b.ints , b.sacks, b.sacked_yards_lost, b.passer_rating, b.rushing_attempts, b.rushing_yards, b.yards_per_carry, b.rushing_tds, b.fumbles, b.fumbles_lost
+from nfl_stats_staging.Quarterback_Game_Logs as b,
+(select distinct player_id, year, season, week from nfl_stats_staging.Quarterback_Game_Logs) as c
+where b.player_id = c.player_id and b.year = c.year and b.season = c.season and b.week = c.week
+order by c.player_id, b.year, b.season, b.week
+
+-- creates new Runningback Game Logs table with only relevent game stats and uses player_id, year, season, game, week as a primary key
+create table nfl_stats_modeled.Runningback_Game_Logs as 
+select distinct c.player_id, b.year, b.season , b.week, b.game_date, b.home_or_away, b.opponent, b.outcome, b.score , b.games_played , b.games_started , b.rushing_attempts , b.rushing_yards , b.yards_per_carry , b.longest_run , b.rushing_tds , b.receptions , b.receiving_yards , b.yards_per_reception , b.longest_reception , b.receiving_tds , b.fumbles , b.fumbles_lost
+from nfl_stats_staging.Runningback_Game_Logs as b,
+(select distinct player_id, year, season, week from nfl_stats_staging.Runningback_Game_Logs) as c
+where b.player_id = c.player_id and b.year = c.year and b.season = c.season and b.week = c.week
+order by c.player_id, b.year, b.season, b.week
+
+-- creates new Receiving Career Stats table with only receiving stats and player_id, year as a primary key
+create table nfl_stats_modeled.Receiving_Career_Stats as 
+select distinct c.player_id, b.year, b.games_played , b.receptions, b.receiving_yards , b.yards_per_reception , b.yards_per_game , b.longest_reception , b.receiving_tds , b.receptions_longer_than_20_yards , b.receptions_longer_than_40_yards , b.first_down_receptions , b.fumbles
+from nfl_stats_staging.Receiving_Career_Stats as b,
+(select distinct player_id, year from nfl_stats_staging.Receiving_Career_Stats) as c
+where b.player_id = c.player_id and b.year = c.year
+order by c.player_id, b.year
+
+-- creates new Rushing Career Stats table with only rushing stats and player_id, year as a primary key
+create table nfl_stats_modeled.Rushing_Career_Stats as 
+select distinct c.player_id, b.year, b.position, b.games_played , b.rushing_attempts , b.rushing_attempts_per_game , b.rushing_yards , b.yards_per_carry , b.rushing_yards_per_game , b.rushing_tds , b.longest_run , b.rushing_first_downs , b.percentage_of_rushing_first_downs , b.rushing_more_than_20_yards, b.rushing_more_than_40_yards, b.fumbles
+from nfl_stats_staging.Rushing_Career_Stats as b,
+(select distinct player_id, year from nfl_stats_staging.Rushing_Career_Stats) as c
+where b.player_id = c.player_id and b.year = c.year
+order by c.player_id, b.year
